1652fee9457cda512bc515533d0d8b1c1fc3fd31
djs0065 <djs0065@mix.wvu.edu>
2021-02-04 20:41:08 -0500

    Lab 2, About Page

---
+++ index.php
@@ -0,0 +1,50 @@
+<body>
+    <link rel="stylesheet" href="css/about.css">
+
+    <div class="about">
+        <h1>About Me</h1>
+        <p>My name is Dawson Smetana and I am a WVU student in CS230 creating a sample movie review website. My favorite movie series is Hunger Games by Suzanne Collins.</p>
+    </div>
+    <div class="row">
+        <div class="column">
+            <div class="card">
+                <img src="images/1.jpg">
+                <div class="container">
+                    <h2>Maze Runner</h2>
+                    <p>James Dashner</p>
+                    <p>2014</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/2.jpg">
+                <div class="container">
+                    <h2>Divergent</h2>
+                    <p>Veronica Roth</p>
+                    <p>2014</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/3.jpg">
+                <div class="container">
+                    <h2>Hunger Games</h2>
+                    <p>Suzanne Collins</p>
+                    <p>2012</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/4.jpg">
+                <div class="container">
+                    <h2>Avengers: End Game</h2>
+                    <p>Stan Lee</p>
+                    <p>2019</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
