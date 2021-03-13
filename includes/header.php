beb5ade23a9305fbdd39f082ed18da6f3f708691
djs0065 <djs0065@mix.wvu.edu>
2021-03-12 22:57:03 -0500

    Lab6

--- includes/header.php
+++ includes/header.php
@@ -27,7 +27,7 @@ session_start();
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
-            <?php if (isset($SESSION['uid'])) {
+            <?php if (isset($_SESSION['uid'])) {
                 echo '<li class="nav-item">
                 <a class="nav-link" href="includes/logout.php">Logout</a>
             </li>
@@ -36,6 +36,9 @@ session_start();
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="profile.php">Profile</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="admin.php">Admin</a>
             </li>';
             }
             else{
@@ -45,7 +48,7 @@ session_start();
             }
             ?>
             <li class="nav-item active">
-                <a class="nav-link" href="../index.php">Home <span class="sr-only">(current)</span></a>
+                <a class="nav-link" href="../index.php">Home<span class="sr-only">(current)</span></a>
             </li>
             </ul>
             <form class="form-inline my-2 my-lg-0">
